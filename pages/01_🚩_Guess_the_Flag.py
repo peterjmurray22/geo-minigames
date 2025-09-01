90be1e7d3e4548af9d78c4e776260e6063772ad4
peterjmurray22 <pjmurray@umich.edu>
2025-08-31 23:33:23 -0500

    Prevent over-rerunning

--- pages/01_🚩_Guess_the_Flag.py
+++ pages/01_🚩_Guess_the_Flag.py
@@ -24,10 +24,11 @@ if "countries" not in st.session_state:
     st.session_state.countries = utils.load_countries()
 if "game_started" not in st.session_state or not st.session_state.game_started:
     pool, num_options, num_rounds = utils.setup_screen(st.session_state.countries)
-    st.session_state.pool = pool
-    st.session_state.num_options = num_options
-    st.session_state.num_rounds = num_rounds
-    st.rerun()
+    if "game_started" in st.session_state and st.session_state.game_started:
+        st.session_state.pool = pool
+        st.session_state.num_options = num_options
+        st.session_state.num_rounds = num_rounds
+        st.rerun()
 else:
     st.metric("Score", st.session_state.score_display)
     utils.run_multiple_choice_game(st.session_state.pool, st.session_state.num_options, st.session_state.num_rounds, 'name', show_flag_question)
