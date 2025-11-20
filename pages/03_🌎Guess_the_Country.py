0fd73953b09ff0668796d2d85345bcef1dad7334
Jacob Gacek <jacobgacek@Jacobs-MacBook-Air.local>
2025-11-19 23:29:59 -0600

    feat: Initializes the guess the country game, checks to see if the image_key is specified before searching for the image path

--- pages/03_🌎Guess_the_Country.py
+++ pages/03_🌎Guess_the_Country.py
@@ -1,27 +1,21 @@
-
 import streamlit as st
-import utils
+import game
+import pathlib
 
 def show_country_question(round_data, multiple_choice=True):
-    answer = round_data["answer"]
-    st.session_state.correct = answer["capital"]
-    if multiple_choice:
-        options = round_data["options"]
-        submitted = utils.show_multiple_choice_options(f"What is the capital of **{answer['name']}**?", options)
-    else:
-        submitted = utils.show_text_entry(f"What is the capital of **{answer['name']}**?")
-    return submitted
+    return game.show_image_question(round_data, pathlib.Path(__file__).resolve().parents[1] / "assets" / "silhouettes", "silhouette", "Which country is this?", multiple_choice)
+
 
 # --- App ---
 st.title("🌎️ Guess the Country")
 
-utils.init_game("captials")
+game.init_game("Guess the Country")
 
 # init global state
 if "countries" not in st.session_state:
-    st.session_state.countries = utils.load_countries()
+    st.session_state.countries = game.load_countries()
 if "game_started" not in st.session_state or not st.session_state.game_started:
-    pool, input, num_options, num_rounds = utils.setup_screen(st.session_state.countries)
+    pool, input, num_options, num_rounds = game.setup_screen(st.session_state.countries)
     if "game_started" in st.session_state and st.session_state.game_started:
         st.session_state.pool = pool
         st.session_state.input = input
@@ -30,4 +24,4 @@ if "game_started" not in st.session_state or not st.session_state.game_started:
         st.rerun()
 else:
     st.metric("Score", st.session_state.score_display)
-    utils.run_game(st.session_state.pool, st.session_state.num_options, st.session_state.num_rounds, 'capital', 'capital_distractors', show_capital_question, verify_distractors=False)
+    game.run_game(st.session_state.pool, st.session_state.num_options, st.session_state.num_rounds, 'name', 'flag_distractors', show_country_question, verify_distractors=False)
