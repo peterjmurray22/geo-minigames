895e105fe4af1fa7ad92475fa45571917d91a7ab
peterjmurray22 <pjmurray@umich.edu>
2025-11-20 02:20:09 -0600

    Fix setup_screen usage in 02 and 03

--- pages/03_🌎Guess_the_Country.py
+++ pages/03_🌎Guess_the_Country.py
@@ -17,13 +17,7 @@ game.init_game("Guess the Country")
 if "countries" not in st.session_state:
     st.session_state.countries = game.load_countries()
 if "game_started" not in st.session_state or not st.session_state.game_started:
-    pool, input, num_options, num_rounds = game.setup_screen(st.session_state.countries)
-    if "game_started" in st.session_state and st.session_state.game_started:
-        st.session_state.pool = pool
-        st.session_state.input = input
-        st.session_state.num_options = num_options
-        st.session_state.num_rounds = num_rounds
-        st.rerun()
+    game.setup_screen(st.session_state.countries)
 else:
     st.metric("Score", st.session_state.score_display)
     game.run_game(st.session_state.pool, st.session_state.num_options, st.session_state.num_rounds, 'name', 'flag_distractors', show_country_question, verify_distractors=False)
